b071ce1d223388550a3820c69f5ca6552b333675
Ritesh Zode <riteshzode1122@gmail.com>
2023-05-17 22:19:56 +0530

    project updated

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = {'xlsx', 'xls'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file.filename == '':
+        return render_template("index.html", error="File is required")
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return render_template("thankyou.html", filename=filename)
+    else:
+        return render_template("index.html", error="Invalid file format")
+
+
+@app.route('/download/<filename>', methods=['GET'])
+def download(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if os.path.exists(file_path):
+        return send_file(file_path, as_attachment=True)
+    else:
+        return 'File not found.'
+
+
+if __name__ == '__main__':
+    app.run()
